d6c75695ac6dd2614e18b27aa1a0dcc3e47796bc
Mudassar <demoncious@gmail.com>
2020-11-11 04:19:04 +0500

    Update routes.php

--- app/themes/default/routes.php
+++ app/themes/default/routes.php
@@ -7,7 +7,7 @@ return [
             $response->setStatus(200)
             ->json($options)
             ->send();
-        }
+        },
     ],
 
     '/api/v1' => require_once(THEME_ROOT . 'src/api/routes.php')
